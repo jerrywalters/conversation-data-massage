4219653a7b9187b1123af47413e380f820ca921e
Jerry Walters <jerrydwalters@gmail.com>
2018-02-19 16:23:27 -0800

    get percentage of inactive users

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,11 +8,13 @@ const Dashboard = () => {
         .map((company)=>{
             let numConversations = users.filter((user)=>{
                 return user.company_id === company.id;
-            }).map((user)=>{
+            })
+            .map((user)=>{
                 return conversations.filter((conversation)=>{
                     return user.email === conversation.from && moment().subtract(numMonths, 'months').diff(conversation.date) < 0;
                 })
-            }).reduce((a,b)=>a+b.length, 0);
+            })
+            .reduce((a,b)=>a+b.length, 0);
             
             return {
                 companyId: company.id,
@@ -21,17 +23,24 @@ const Dashboard = () => {
         })
     }
 
+    console.time('sort')
     function rankCompaniesByAmountOfMonths(companies, conversations, users, numMonths) {
         return getCompaniesConversationsByMonth(companies, conversations, users, numMonths)
-        .sort((a, b) => b.numConversations - a.numConversations);
+        .sort((a, b) => b.numConversations - a.numConversations)
+        .slice(0,5);
     }
+    console.log(rankCompaniesByAmountOfMonths(companies, conversations, users, 6))
+    console.timeEnd('sort')
 
+    console.time('reduce')
     function getTotalConversationsByMonth(companies, conversations, users, numMonths) {
         return getCompaniesConversationsByMonth(companies, conversations, users, numMonths)
         .reduce((a, b)=>{
             return a + b.numConversations
         }, 0)
     }
+    console.log(getTotalConversationsByMonth(companies, conversations, users, 3 ))
+    console.timeEnd('reduce')
 
     function getTopFiveUsers(companies, conversations, users) {
         return companies.map((company)=>{
@@ -85,6 +94,35 @@ const Dashboard = () => {
             return Object.values(b)[0] - Object.values(a)[0]
         });
 
+    console.time('inactive')
+    function getInactiveUsersByMonth(companies, conversations, users, numMonths) {
+        return companies
+        .map((company)=>{
+            let userConversations = users.filter((user)=>{
+                return user.company_id === company.id;
+            })
+            .map((user)=>{
+                return conversations.filter((conversation)=>{
+                    return user.email === conversation.from && moment().subtract(numMonths, 'months').diff(conversation.date) < 0;
+                })
+            })
+
+            const numUsers = userConversations.length;
+            const numInactive = userConversations.filter(convo=>convo.length===0).length;
+
+            return {
+                companyId: company.id,
+                percentInactive: getPercentage(numUsers, numInactive),
+            };
+        })
+    }
+
+    function getPercentage(total, diff) {
+        return (diff/total)*100;
+    }
+
+    console.log('inactive', getInactiveUsersByMonth(companies, conversations, users, 4))
+    console.timeEnd('inactive')
     // takes duplicates and stores them in an object with the name as the key and a value of the number of duplicates
     function groupByDuplicates(acc, value) {
         !acc[value] ? acc[value] = 1 : acc[value] += 1
